8938390cce6a1ddee75dbadaa64e5fdcf3d12554
zooduck <zooduck@beniceoreatsomerice.com>
2019-10-22 21:48:56 +0100

    update reference to test mongodb collection

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,13 +1,13 @@
 const resolvers = (db) => {
     return {
         async animal(args) {
-            const result = await db.collection('test').findOne({id: args.id});
+            const result = await db.collection('__test').findOne({id: args.id});
 
             return result;
         },
         async animals(args) {
             const options = args.size ? { size: args.size } : {};
-            const result = await db.collection('test').find(options).toArray();
+            const result = await db.collection('__test').find(options).toArray();
 
             return result;
         }
